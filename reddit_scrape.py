16f4152f5b852a0e738e89bf8330d3463125d05e
rkrabek <rkrabek@gmail.com>
2015-10-21 13:39:56 -0400

    added comments

--- reddit_scrape.py
+++ reddit_scrape.py
@@ -1,18 +1,20 @@
 import xml.etree.cElementTree as ET
-import urllib2
 from bs4 import BeautifulSoup
 
+# parse reddit front page html
 reddit = urllib2.urlopen('http://www.reddit.com/')
 soup = BeautifulSoup(reddit, 'html.parser')
-test = soup.find("p")
+
+# find top post by finding rank 1 and then appropriate sibling sub-tag
 fp_ranks = soup.findAll("span", class_="rank")
 top_rank = fp_ranks[len(fp_ranks)%25]
 top_post = top_rank.parent.find("a", class_="title")
 
+# creates TwiML document with name and top post
 response = ET.Element("Response")
-
 ET.SubElement(response, "Say", voice="alice").text = "Robert Krabek"
 ET.SubElement(response, "Say", voice="alice").text = top_post.string
 
+# writes TwiML file to Dropbox Public directory accessible to autocall.py
 file = ET.ElementTree(response)
 file.write("../../Public/response.xml")
