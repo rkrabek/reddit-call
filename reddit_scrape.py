157563571464351f00cf73f5ccbca4fdc59245ca
rkrabek <rkrabek@gmail.com>
2015-10-21 02:33:06 -0400

    xml writes to Public dropbox directory

---
+++ reddit_scrape.py
@@ -0,0 +1,18 @@
+import xml.etree.cElementTree as ET
+import urllib2
+from bs4 import BeautifulSoup
+
+reddit = urllib2.urlopen('http://www.reddit.com/')
+soup = BeautifulSoup(reddit, 'html.parser')
+test = soup.find("p")
+fp_ranks = soup.findAll("span", class_="rank")
+top_rank = fp_ranks[len(fp_ranks)%25]
+top_post = top_rank.parent.find("a", class_="title")
+
+response = ET.Element("Response")
+
+ET.SubElement(response, "Say", voice="alice").text = "Robert Krabek"
+ET.SubElement(response, "Say", voice="alice").text = top_post.string
+
+file = ET.ElementTree(response)
+file.write("../../Public/response.xml")
